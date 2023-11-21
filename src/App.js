b78fcfb86243abcf1d00cc234011d3b165dd4d75
dicepy <mdi.2005@mail.ru>
2023-11-21 21:39:26 +0500

    select+form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, {useRef, useState} from "react";
+import React, {useMemo, useRef, useState} from "react";
 import './styles/App.css'
 import PostList from "./components/PostList";
-import MyButton from "./components/UI/button/MyButton";
+import PostForm from "./components/PostForm";
+import MySelect from "./components/UI/select/MySelect";
 import MyInput from "./components/UI/input/MyInput";
 
 function App() {
@@ -11,30 +12,61 @@ function App() {
     {id:2, title: "Javascript", body:"Description"},
     {id:3, title: "Javascript", body:"Description"}
   ])
-  const [title,setTitle] = useState('')
-  const [body,setBody] = useState('')
-  const addNewPost = (e) => {
-    e.preventDefault();
-    const newPost = {
-      id: Date.now(),
-      title,
-      body
+ 
+  const [selectedSort, setSelectedSort] = useState('')
+  const [searchQuery, setSearchQuery] = useState('')
+
+
+
+  const sortedPosts = useMemo(() => {
+    console.log(1)
+    if(selectedSort){
+      return [...posts].sort((a,b) => a[selectedSort].localeCompare(b[selectedSort]))
     }
-    setPosts([...posts, newPost])
-    setTitle('')
-    setBody('')
+    return posts;
+  }, [selectedSort, posts])
+
+  const createPost = (newPost) => {
+    setPosts([...posts,newPost])
+  }
+
+  const removePost = (post) => {
+    setPosts(posts.filter(p => p.id !== post.id))
+  }
+
+  const sortPosts = (sort) => {
+    setSelectedSort(sort)
   }
 
   return (
     
     <div className="App">
-      <form>
-        <MyInput value={title} onChange={e => setTitle(e.target.value)} type="text" placeholder="Название поста" />
-        <MyInput value={body} onChange={e => setBody(e.target.value)}
-        type="text" placeholder="Описание поста" />
-        <MyButton  onClick={addNewPost}>Создать пост</MyButton>
-      </form>
-      <PostList posts={posts} title="Список постов 1"/>
+      <PostForm create={createPost}/>
+      <hr style={{margin: '15px 0'}}/>
+      <div>
+        <MyInput 
+        value={searchQuery}
+        onChange={e => setSearchQuery(e.target.value)}
+          placeholder='Поиск...'
+        />
+        <MySelect
+          value={selectedSort}
+          onChange={sortPosts}
+          defaultValue="Сортировка"
+          options={[
+
+            {value: 'title', name: 'По названию'},
+            {value: 'body', name: 'По описанию'},
+          ]}
+        />
+      </div>
+      {posts.length !== 0
+      ? <PostList remove={removePost} posts={sortedPosts} title="Список постов 1"/>
+      : <h1 style={{textAlign:'center'}}>
+        Посты не найдены!
+        </h1>
+      }
+      
     </div>
   );
 }
--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import MyButton from './UI/button/MyButton'
 
 export default function PostItem(props) {
   return (
@@ -10,7 +11,7 @@ export default function PostItem(props) {
             </div>
         </div>
         <div className="post__btns">
-            <button>Удалить</button>
+            <MyButton onClick={() => props.remove(props.post)}>Удалить</MyButton>
         </div>
     </div>
   )
