15cfcecf3de22988b4c8ae993dd7b33d52f88599
dicepy <mdi.2005@mail.ru>
2023-12-10 18:48:48 +0300

    useFetching

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import MyButton from "./components/UI/button/MyButton";
 import {usePosts} from "./hooks/usePosts";
 import PostService from "./API/PostService";
 import Loader from "./components/UI/Loader/Loader";
+import {useFetching} from "./hooks/useFetching";
 
 function App() {
 
@@ -17,7 +18,10 @@ function App() {
   const [filter, setFilter] = useState({sort: '', query: ''})
   const [modal, setModal] = useState(false)
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
-  const [isPostsLoading, setIsPostsLoading] = useState(false);
+  const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+      const posts = await PostService.getAll();
+      setPosts(posts)
+  })
 
   useEffect(()=> {
       fetchPosts();
@@ -31,16 +35,6 @@ function App() {
     setModal(false)
   }
 
-  async function fetchPosts() {
-
-      setIsPostsLoading(true);
-      setTimeout(async () => {
-          const posts = await PostService.getAll();
-          setPosts(posts)
-          setIsPostsLoading(false);
-      }, 1000)
-
-  }
 
   const removePost = (post) => {
     setPosts(posts.filter(p => p.id !== post.id))
@@ -59,6 +53,8 @@ function App() {
 
       <hr style={{margin: '15px 0'}}/>
       <PostFilter filter={filter} setFilter={setFilter}/>
+        {postError &&
+        <h1>Произошла ошибка ${postError}</h1>}
         {
             isPostsLoading
                 ? <div style={{display:'flex', justifyContent: 'center', marginTop:'50px'}}><Loader/></div>
