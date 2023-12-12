7be546abaffed3f2c8bda49e408846cb6b13dddf
dicepy <mdi.2005@mail.ru>
2023-12-12 09:57:46 +0300

    pagination ended

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import PostService from "./API/PostService";
 import Loader from "./components/UI/Loader/Loader";
 import {useFetching} from "./hooks/useFetching";
 import {getPageCount, getPagesArray} from "./utils/pages";
+import Pagination from "./components/UI/pagination/Pagination";
 
 function App() {
 
@@ -22,9 +23,8 @@ function App() {
   const [limit, setLimit] =   useState(10);
   const [page,setPage] = useState(1);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
-    let pagesArray = getPagesArray(totalPages)
 
-  const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+  const [fetchPosts, isPostsLoading, postError] = useFetching(async (limit, page) => {
       const response = await PostService.getAll(limit,page);
       setPosts(response.data)
       const totalCount = response.headers['x-total-count']
@@ -33,11 +33,13 @@ function App() {
 
 
   useEffect(()=> {
-      fetchPosts();
-  }, [])
+      fetchPosts(limit, page);
+  }, [page])
 
-
-
+  const changePage = (page) => {
+      setPage(page)
+      fetchPosts(limit, page)
+  }
 
   const createPost = (newPost) => {
     setPosts([...posts,newPost])
@@ -69,13 +71,7 @@ function App() {
                 ? <div style={{display:'flex', justifyContent: 'center', marginTop:'50px'}}><Loader/></div>
                 : <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Список постов 1"/>
         }
-        <div className="page__wrapper">
-            {pagesArray.map(p =>
-                <span className={page === p ? "page page__current" : "page"}>
-                    {p}
-                </span>
-            )}
-        </div>
+        <Pagination page={page} changePage={changePage} totalPages={totalPages}/>
     </div>
   );
 }
